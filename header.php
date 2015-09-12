94e2e241a4be49b9cbf40bd0727b75f9a2b18313
mlloewen <mlloewen@gmail.com>
2015-09-11 21:11:49 -0600

    a css menu experiment

--- header.php
+++ header.php
@@ -34,10 +34,10 @@
             <p class="site-description"><?php bloginfo( 'description' ); ?></p>
         </div><!-- .site-branding -->
 
-        <nav id="site-navigation" class="main-navigation" role="navigation">
+        <nav id="site-navigation" class="main-navigation main-menu" role="navigation">
             <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'ml-base' ); ?></button>
             <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-        </nav><!-- #site-navigation -->
+        </nav> <!-- #site-navigation -->
     </header><!-- #masthead -->
 
     <div id="content" class="site-content">
